5d75e779f75fdc7ab5b77aeb7cba348a8573cc63
APEX <anweshshrestha809@gmail.com>
2026-07-23 21:27:16 +0545

    Add files via upload

---
+++ c_assigment/c_loops/question_6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i; int b;
+    printf(" Enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<n;i++){
+        if(i*i<=n ){
+            if((i*i)%2==0)
+        printf("\n%d",i*i);}
+        }
+    }
